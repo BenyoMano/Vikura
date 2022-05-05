57b05bd9e7e7bedb374e085c5a13a0e7699abd3f
Benjamin Berg <berg.benjamin.z@gmail.com>
2022-05-05 19:16:33 +0200

    added auth sub

--- src/components/Hem.js
+++ src/components/Hem.js
@@ -1,36 +1,77 @@
-import React from "react";
-import { View, StyleSheet, Platform, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Platform, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Button from "./Button";
 import InputBar from "./InputBar";
 import Logo from "./Logo";
 import Welcome from './Welcome';
-//import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import auth from '@react-native-firebase/auth';
 
+function Initiate() {
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
-/* //const phoneNumber = getPhoneNumberFromUserInput();
-const appVerifier = window.recaptchaVerifier;
-const auth = getAuth();
-//auth.languageCode = 'it';
-firebase.auth().useDeviceLanguage();
-
-window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
-    'size': 'invisible',
-    'callback': (response) => {
-        // reCATPCHA solved, allow signInWithPhoneNumber.
-        onSignInSubmit();
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
     }
-}, auth);
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; //unsubscribe on unmount
+    }, []);
+    
+    if (initializing) return null;
 
-signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-    }).catch((error) => {
-        //Error, sms not sent
-        //...
-    }); */
+    if(!user) {
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+        );
+    }
+    return (
+        <View>
+            <Text>Welcome {user.phoneNumber}</Text>
+        </View>
+    );
+}
 
+function PhoneSignIn() {
+    // If null, no SMS has been sent
+    const [confirm, setConfirm] = useState(null);
+    const [code, setCode] = useState('');
+    console.log('PhoneSignIn');
+    // Handle the button press
+    async function signInWithPhoneNumber(phoneNumber) {
+        console.log('await auth.signInWPN(pn)')
+        const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+        setConfirm(confirmation);
+    }
+    async function confirmCode() {
+        try {
+            console.log('await confirm.confirm(code)');
+            await confirm.confirm(code);
+            
+        } catch (error) {
+            console.log('Invalid code.');
+        }
+    }
+    if (!confirm) {
+        return (
+            <Button
+                title='Phone Number Sign In'
+                onPress={() => signInWithPhoneNumber('+46725180309')}
+                />
+        );
+    }
+    return (
+        <>
+            <TextInput value={code} onChangeText={text => setCode(text)} />
+            <Button title='Confirm Code' onPress={() => confirmCode()} />
+        </>
+    );
+}
 
 const Hem = ({ navigation }) => {
 
@@ -47,8 +88,14 @@ const Hem = ({ navigation }) => {
                     <View>
                         <Logo style={{ width: 160, height: 62, marginTop: 90 }}/>
                     </View>
-                    <View style={{flex: 1}}>
+                   {/*  <View style={{flex: 1}}>
                         <Welcome title='Välkommen!' style={{ fontSize: 42, marginTop:-10 }} />
+                    </View> */}
+                    <View style={{flex: 1}}>
+                        <Initiate />
+                    </View>
+                    <View style={{flex:2}}>
+                        <PhoneSignIn />
                     </View>
                     <View style={{flex: 1}}>
                         <InputBar title='Kod:' />

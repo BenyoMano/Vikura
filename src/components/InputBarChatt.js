cbdb7b0e1981e6c0f401fab03696d63de3d929b2
Benjamin Berg <berg.benjamin.z@gmail.com>
2022-06-09 13:05:39 +0200

    fixad design InputBar

--- src/components/InputBarChatt.js
+++ src/components/InputBarChatt.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput, View } from 'react-native';
 
 const InputBarChatt = (props) => {
 const { viewStyle, barStyle } = styles;
+const [text, onChangeText] = useState();
 
     return (
         <View style={viewStyle}>
-            <TextInput style={barStyle} />
+            <TextInput style={barStyle} 
+            onChangeText={onChangeText} 
+            value={text} 
+            multiline
+            placeholder="Skriv något..." 
+            placeholderTextColor="grey" 
+            textBreakStrategy='simple'
+            underlineColorAndroid='transparent'
+            >
+            </TextInput>
         </View>
     );
 }
@@ -16,17 +26,18 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 0,
-        
     },
     barStyle: {
-        height: 52,
+        minHeight: 52,
+        maxHeight: 250,
         width: 244,
         color: 'black',
         backgroundColor: '#EEEEEE',
         borderColor: 'gray',
         borderWidth: 2,
         borderRadius: 12,
-        fontFamily: 'NunitoSans-Regular'
+        padding: 10,
+        fontFamily: 'NunitoSans-Regular',
     }
 }
 

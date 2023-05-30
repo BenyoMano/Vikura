6634f98dac1661a72480b55bf5133ad6f6e638ab
Jonathan <jonathan.wedin@nordnet.se>
2023-05-30 18:48:26 +0200

    Clean up createUser async.

--- src/components/AddUser/createUser.js
+++ src/components/AddUser/createUser.js
@@ -1,6 +1,6 @@
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-import {showMessage} from 'react-native-flash-message';
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import { showMessage } from "react-native-flash-message";
 
 const createUser = ({
   userPropToAdd,
@@ -18,55 +18,55 @@ const createUser = ({
 
   if (!userPropToAdd.firstName) {
     showMessage({
-      message: 'Varning!',
-      description: 'Namn saknas!',
-      type: 'danger',
+      message: "Varning!",
+      description: "Namn saknas!",
+      type: "danger",
       duration: 2500,
     });
     return;
   }
   if (!userPropToAdd.secondName) {
     showMessage({
-      message: 'Varning!',
-      description: 'Efternamn saknas!',
-      type: 'danger',
+      message: "Varning!",
+      description: "Efternamn saknas!",
+      type: "danger",
       duration: 2500,
     });
     return;
   }
   if (!userPropToAdd.mejl) {
     showMessage({
-      message: 'Varning!',
-      description: 'Mejl saknas!',
-      type: 'danger',
+      message: "Varning!",
+      description: "Mejl saknas!",
+      type: "danger",
       duration: 2500,
     });
     return;
   }
   if (!userPropToAdd.password) {
     showMessage({
-      message: 'Varning!',
-      description: 'Lösenord saknas!',
-      type: 'danger',
+      message: "Varning!",
+      description: "Lösenord saknas!",
+      type: "danger",
       duration: 2500,
     });
     return;
   }
   if (!userPropToAdd.personnummer) {
     showMessage({
-      message: 'Varning!',
-      description: 'Personnummer saknas!',
-      type: 'danger',
+      message: "Varning!",
+      description: "Personnummer saknas!",
+      type: "danger",
       duration: 2500,
     });
     return;
   }
   if (userPropToAdd.personnummer.length !== 12) {
     showMessage({
-      message: 'Varning!',
+      message: "Varning!",
       description:
-        'Personnummret måste innehålla 12 siffror, utan bindestreck!',
-      type: 'danger',
+        "Personnummret måste innehålla 12 siffror, utan bindestreck!",
+      type: "danger",
       duration: 3500,
     });
     return;
@@ -79,9 +79,9 @@ const createUser = ({
     userPropToAdd.password &&
     userPropToAdd.personnummer
   ) {
-    const addPersonalDetails = async user => {
-      const refUID = firestore().collection('Users').doc(user.uid);
-      const userAlias = checkboxStateKurator ? 'KURATOR' : '';
+    const addPersonalDetails = async (user) => {
+      const refUID = firestore().collection("Users").doc(user.uid);
+      const userAlias = checkboxStateKurator ? "KURATOR" : "";
       await refUID.set({
         firstName: userPropToAdd.firstName,
         secondName: userPropToAdd.secondName,
@@ -95,44 +95,43 @@ const createUser = ({
     };
 
     async function createNewUser() {
-      await auth()
-        .createUserWithEmailAndPassword(
+      try {
+        await auth().createUserWithEmailAndPassword(
           userPropToAdd.mejl,
-          userPropToAdd.password,
-        )
-        .then(() => {
-          const user = auth().currentUser;
+          userPropToAdd.password
+        );
 
-          addPersonalDetails(user).then(() =>
-            auth()
-              .signOut()
-              .then(() => {
-                setUserPropToAdd({
-                  firstName: '',
-                  secondName: '',
-                  mejl: '',
-                  password: '',
-                  personnummer: '',
-                  kurator: '',
-                });
-                setHasAddedUser(true);
-                showMessage({
-                  message: 'Kontot skapades framgångsrikt!',
-                  type: 'success',
-                  duration: 2000,
-                });
-              }),
-          );
-        })
-        .catch(error => {
-          showMessage({
-            message: 'Misslyckades!',
-            description: String(error),
-            type: 'danger',
-            duration: 5000,
-          });
+        const user = auth().currentUser;
+
+        await addPersonalDetails(user);
+
+        await auth().signOut();
+
+        setUserPropToAdd({
+          firstName: "",
+          secondName: "",
+          mejl: "",
+          password: "",
+          personnummer: "",
+          kurator: "",
         });
-      setSubmitted(false);
+
+        setHasAddedUser(true);
+
+        showMessage({
+          message: "Kontot skapades framgångsrikt!",
+          type: "success",
+          duration: 2000,
+        });
+      } catch (error) {
+        showMessage({
+          message: "Misslyckades!",
+          description: String(error),
+          type: "danger",
+          duration: 5000,
+        });
+        setSubmitted(false);
+      }
     }
     createNewUser();
   }

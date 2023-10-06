dee3ce4453104003e6fe2375c702f9dace6d1433
Benjamin Berg <berg.benjamin.z@gmail.com>
2023-10-06 16:08:29 +0200

    fixed useRoomsData

--- src/components/KuratorOffice/useRoomsData.js
+++ src/components/KuratorOffice/useRoomsData.js
@@ -1,25 +1,30 @@
-import { useState, useEffect } from 'react';
-import getAllRoomNames from '../../firebase/getAllRoomNames';
+import {useState, useEffect} from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 export const useRoomsData = ({setIsLoaded}) => {
+  const [rooms, setRooms] = useState([]);
 
-    const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    let unsubscribeFromAllRoomNames;
+    const fetchRooms = async () => {
+      unsubscribeFromAllRoomNames = firestore()
+        .collection('rooms')
+        .where('users.client.id', '!=', '')
+        .onSnapshot(roomNames => {
+          const newRooms = roomNames.docs.map(roomDoc => {
+            const {alias: clientAlias, id: clientId} =
+              roomDoc.data().users.client;
+            const latestTimestamp = roomDoc.data().latestTimestamp;
+            const roomId = roomDoc.id;
+            return {roomId, clientAlias, clientId, latestTimestamp};
+          });
+          setRooms(newRooms);
+        });
+      setIsLoaded(true);
+    };
+    fetchRooms();
+    return () => unsubscribeFromAllRoomNames();
+  }, []);
 
-    useEffect(()=>{
-        const fetchRooms = async () => {
-            const roomNamesSnapshot = await getAllRoomNames();
-            const newRooms = roomNamesSnapshot.docs.map(roomDoc=>{
-                const {alias: clientAlias, id: clientId} = roomDoc.data().users.client;
-                const latestTimestamp = roomDoc.data().latestTimestamp;
-                const roomId = roomDoc.id;
-                return {roomId, clientAlias, clientId, latestTimestamp}
-            });
-            setRooms(newRooms);
-            setIsLoaded(true);
-        }
-        fetchRooms()
-    }, [])
-
-    return rooms;
-
-}
+  return rooms;
+};
